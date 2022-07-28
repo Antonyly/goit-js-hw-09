813f076c7afc50d45f3cfd9e8cc3b5a36501577d
Antonyly <97746965+Antonyly@users.noreply.github.com>
2022-07-28 13:59:15 +0000

    Deploying to gh-pages from @ Antonyly/goit-js-hw-09@aff3141d521f3fa9384c72ad6d0614d1d6f22495 🚀

---
+++ 01-color-switcher.139a3e6d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=0;var d=function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))};e.disabled=!0,t.addEventListener("click",(function(){a=setInterval(d,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.139a3e6d.js.map
