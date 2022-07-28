aff3141d521f3fa9384c72ad6d0614d1d6f22495
Antonyly <antonlyzun@gmail.com>
2022-07-28 16:58:43 +0300

    +

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,54 @@
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+
+let timerId = 0;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const backgroundColorSwitcher = function() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+btnStopEl.disabled = true;
+btnStartEl.addEventListener('click', () => {
+    timerId = setInterval(backgroundColorSwitcher, 1000)
+    btnStartEl.disabled = true;
+    btnStopEl.disabled = false;
+});
+btnStopEl.addEventListener('click', () => {
+    clearInterval(timerId);
+    btnStartEl.disabled = false;
+    btnStopEl.disabled = true;
+})
+
+
+
+
+
+
+// let timerId = null;
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// // function to switch color to random
+// btnStopEl.disabled = true;
+// const backgroundColorSwitcher = function () {
+//     document.body.style.backgroundColor = getRandomHexColor();
+// };
+
+// // start changing color
+
+// btnStartEl.addEventListener('click', () => {
+//   timerId = setInterval(backgroundColorSwitcher, 1000);
+//     btnStartEl.disabled = true;
+//     btnStopEl.disabled = false;
+// });
+
+// // stop changing color
+
+// btnStopEl.addEventListener('click', () => {
+//   clearInterval(timerId);
+//     btnStartEl.disabled = false;
+//     btnStopEl.disabled = true;
+// });
 
---
+++ src/js/02-timer.js
@@ -0,0 +1,94 @@
+import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const buttonStartEl = document.querySelector('[data-start]');
+const inputEl = document.querySelector('#datetime-picker');
+const dEl = document.querySelector('[data-days]');
+const hEl = document.querySelector('[data-hours]');
+const mEl = document.querySelector('[data-minutes]');
+const sEl = document.querySelector('[data-seconds]');
+
+buttonStartEl.addEventListener('click', () => timer.start());
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+let selectedTime = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] < new Date()) {
+            Notify.failure('Please choose a date in the future');
+            selectedDates[0] = new Date();
+        } else {
+            buttonStartEl.disabled = false;
+            selectedTime = selectedDates[0];
+      }
+    console.log(selectedDates[0]);
+  },
+};
+
+class Timer {
+    constructor() {
+        this.timerID = null;
+        this.isActive = false;
+        buttonStartEl.disabled = true;
+    }
+    start() {
+        if(this.isActive) {
+        return;
+        }
+        this.isActive = true;
+        this.timerID = setInterval(() => {
+        const thisTime = Date.now();
+        const averageTime = selectedTime - thisTime;
+        const componentsTimer = convertMs(averageTime);
+        this.updateComponentsTimer(componentsTimer);
+        if (averageTime <= 0) {
+            this.stopTimer();
+        }
+        }, 1000);
+    }
+    updateComponentsTimer({days, hours, minutes, seconds}) {
+        dEl.textContent = addLeadingZero(days);
+        hEl.textContent = addLeadingZero(hours);
+        mEl.textContent = addLeadingZero(minutes);
+        sEl.textContent = addLeadingZero(seconds);
+
+    }
+    stopTimer() {
+        clearInterval(this.timerID);
+  }
+}
+
+const timer = new Timer();
+flatpickr(inputEl, options);
